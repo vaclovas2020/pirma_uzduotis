9c0a57fc6e30bc7ad14be9209a80223959e6e906
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-01 14:54:00 +0300

    Added word_hyphenation CLI PHP script

---
+++ word_hyphenation.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+WORD HYPHENATION PHP CLI
+$word - given word
+$data - hyphenation patterns
+*/
+function word_hyphenation($word, $data){ 
+    
+}
+if ($argc == 2){
+    $word = $argv[1];
+}
+else{
+    echo "Please give one word. Use command 'php word_hyphenation.php word'";
+}
+?>
