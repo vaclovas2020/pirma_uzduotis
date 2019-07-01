923afdcdd980624824bc1f4e62eb8c82b8ff9f61
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-01 17:04:26 +0300

    try to find patterns

--- word_hyphenation.php
+++ word_hyphenation.php
@@ -8,11 +8,30 @@ require_once('function.read_data.php');
 require_once('function.print_result.php');
 
 function word_hyphenation($word, $data){ 
-    
+    $result = array();
+    foreach ($data as $pattern){
+        $no_counts = preg_replace('/[0-9]+/', '',$pattern);
+        $begin = false;
+        if (substr($pattern,0,1) == '.'){
+            $begin = true;
+        }
+        if ($begin){
+            if (strpos($word, substr($no_counts, 1)) === 0){
+                array_push($result, $pattern);
+            }
+        }
+        else{
+            if (strpos($word, $no_counts) !== false){
+                array_push($result, $pattern);
+            }
+        }
+    }
+    return $result;
 }
 if ($argc == 2){
     $word = $argv[1];
-    $result_array = word_hyphenation($word, read_data('tex-hyphenation-patterns.txt'));
+    $data = read_data('tex-hyphenation-patterns.txt');
+    $result_array = word_hyphenation($word, $data);
     print_result($result_array);
 }
 else{
