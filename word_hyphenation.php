e14b4684212b83a11d780d5d693122b55f39e6e5
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-01 15:01:38 +0300

    Get result array and print

--- word_hyphenation.php
+++ word_hyphenation.php
@@ -4,11 +4,16 @@ WORD HYPHENATION PHP CLI
 $word - given word
 $data - hyphenation patterns
 */
+require_once('function.read_data.php');
+require_once('function.print_result.php');
+
 function word_hyphenation($word, $data){ 
     
 }
 if ($argc == 2){
     $word = $argv[1];
+    $result_array = word_hyphenation($word, read_data('tex-hyphenation-patterns.txt'));
+    print_result($result_array);
 }
 else{
     echo "Please give one word. Use command 'php word_hyphenation.php word'";
