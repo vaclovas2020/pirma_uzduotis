ee73b18019b109e8450f82517f8ac10ff8cf152b
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-02 15:38:12 +0300

    don't print '-' at begin

--- function.print_result.php
+++ function.print_result.php
@@ -1,12 +1,16 @@
 <?php function print_result($result_array){
+    $el_count = 0;
     foreach ($result_array as $char_data){
         $char = $char_data['char'];
         $count = $char_data['count'];
         if (!empty($count)){
             if ($count % 2 > 0){
-                echo '-';
+                if ($el_count > 0){
+                    echo '-';
+                }
             }
         }
         echo "$char";
+        $el_count++;
     }
 } ?>
