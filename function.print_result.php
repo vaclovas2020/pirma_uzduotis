59780b5c302d467966b2999b46a39c65ef971cb4
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-02 15:18:37 +0300

    change odd nuber to - and print result

--- function.print_result.php
+++ function.print_result.php
@@ -1,3 +1,12 @@
 <?php function print_result($result_array){
-var_dump($result_array); //test
+    foreach ($result_array as $char_data){
+        $char = $char_data['char'];
+        $count = $char_data['count'];
+        if (!empty($count)){
+            if ($count % 2 > 0){
+                echo '-';
+            }
+        }
+        echo "$char";
+    }
 } ?>
