e075a5b5c1218dd17b59b734de0f6dbe4032aefa
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-03 14:52:11 +0300

    echo changed to string return

--- function.print_result.php
+++ function.print_result.php
@@ -1,16 +1,18 @@
 <?php function print_result($result_array){
     $el_count = 0;
+    $str = '';
     foreach ($result_array as $char_data){
         $char = $char_data['char'];
         $count = $char_data['count'];
         if (!empty($count)){
             if ($count % 2 > 0){
                 if ($el_count > 0){
-                    echo '-';
+                    $str .= '-';
                 }
             }
         }
-        echo "$char";
+        $str .= "$char";
         $el_count++;
     }
+    return $str;
 } ?>
