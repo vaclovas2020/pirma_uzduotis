ec02ca150834edb7d1394f45e3bbe3df181b0ae0
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 17:09:51 +0300

    added attribute $position_at_word

--- classes/Hyphenation\WordChar/Hyphenation\WordChar.php
+++ classes/Hyphenation\WordChar/Hyphenation\WordChar.php
@@ -3,9 +3,16 @@
 namespace Hyphenation;
 
 class WordChar extends HyphenationChar{
+    private $position_at_word = 0;
+
+    public function __construct(string $char = '', int $count = 0, int $position_at_word = 0){
+        parent::__construct();
+        $this->$position_at_word = $position_at_word;
+    }
+
     public function toString(): string{
         $str = '';
-        if ($this->$count % 2 > 0){
+        if ($this->$count % 2 > 0 && $this->$position_at_word > 0){
             $str .= '-';
         }
         $str .= $this->$char;
