631eb28589127a02a03310631b026c99801c62dc
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 16:58:24 +0300

    added ResultPrinter class

---
+++ classes/IO\ResultPrinter/IO\ResultPrinter.php
@@ -0,0 +1,21 @@
+<?php namespace IO;
+
+class ResultPrinter implements iFileWriter{
+    private $result_str = '';
+
+    public function _construct(array $result_array){
+        $el_count = 0;
+        foreach ($result_array as $char_data){
+            $this->$result_str .= $char_data->toString();
+        }
+    }
+
+    public function writeToFile(string $filename): bool{
+        return file_put_contents($filename,$result_str) !== false;
+    }
+
+    public function printToScreen(){
+        echo $this->$result_str;
+    }
+
+}
