915d7fded1d31b7efd907d0d108d5a41e591059e
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 18:22:00 +0300

    added class CLI\ExecDurationCalculator

---
+++ classes/CLI\ExecDurationCalculator/CLI\ExecDurationCalculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CLI;
+
+class ExecDurationCalculator{
+    private $begin_time = 0.0;
+    private $end_time = 0.0;
+    public function start(){
+        $this->$begin_time = microtime(true);
+    }
+    public function finish(){
+        $this->$end_time = microtime(true);
+    }
+    function getDuration(): float{
+        return $this->$end_time - $this->$begin_time;
+    }
+}
