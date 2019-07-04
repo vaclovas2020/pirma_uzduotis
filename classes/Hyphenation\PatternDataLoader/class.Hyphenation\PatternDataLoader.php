52f689d020a4a992e38cf30af9d6efe274186f89
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 13:26:39 +0300

    create pattern_data array

--- classes/Hyphenation\PatternDataLoader/class.Hyphenation\PatternDataLoader.php
+++ classes/Hyphenation\PatternDataLoader/class.Hyphenation\PatternDataLoader.php
@@ -3,9 +3,8 @@
 namespace Hyphenation;
 
 class PatternDataLoader{
-    static private $pattern_data;
+    static private $pattern_data = array();
     static public function loadDataFromFile(string $filename){
-        self::$pattern_data = array();
         $file = new \SplFileObject($filename, 'r');
         while (!$file->eof()){
             array_push(self::$pattern_data, str_replace("\n",'',$file->fgets()));
