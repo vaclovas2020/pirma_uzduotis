b7939b0212519eaf2a558cfef088f537948eded5
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 16:39:27 +0300

    deleted function read_data

--- function.read_data.php
+++
@@ -1,8 +0,0 @@
-<?php function read_data(string $filename){
-    $data = array();
-    $file = new SplFileObject($filename, 'r');
-    while (!$file->eof()){
-        array_push($data, str_replace("\n",'',$file->fgets()));
-    }
-    return $data;
-} ?>
