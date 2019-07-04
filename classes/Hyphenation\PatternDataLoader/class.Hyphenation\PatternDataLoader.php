dd1a79ae0d27e7b40cd7fade6a6da884653be3d5
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-04 12:03:25 +0300

    renamed class

--- classes/Hyphenation\PatternDataLoader/class.Hyphenation\PatternDataLoader.php
+++ classes/Hyphenation\PatternDataLoader/class.Hyphenation\PatternDataLoader.php
@@ -1,10 +1,17 @@
-<?php namespace hyphenation;
+<?php 
+
+namespace Hyphenation;
+
 class PatternDataLoader{
     static private $pattern_data;
     static public function loadDataFromFile(string $filename){
-        $file = new SplFileObject($filename, 'r');
+        self::$pattern_data = array();
+        $file = new \SplFileObject($filename, 'r');
         while (!$file->eof()){
             array_push(self::$pattern_data, str_replace("\n",'',$file->fgets()));
         }
+    }
+    static public function getPatternData(): array{
+        return self::$pattern_data;
     }
 }
