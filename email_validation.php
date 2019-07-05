700e20f2a8298ec156ea483c889aa1fb02dd0b52
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-05 11:52:38 +0300

    updated email_validation.php script to object oriented

--- email_validation.php
+++ email_validation.php
@@ -1,17 +1,25 @@
-<?php function email_validation(string $email){
-    return preg_match('/[a-z._0-9]+[@]{1}[a-z._0-9]+[.]{1}[a-z0-9]{2,}/i',$email) === 1;
-}
-function main(){
-    global $argc;
-    global $argv;
-    if ($argc == 2){
-        $email = $argv[1];
-        if (email_validation($email)){
-            echo "Email '$email' is valid!";
+<?php
+namespace Validation;
+
+class EmailValidator
+{
+    private static function email_validation(string $email): bool
+    {
+        return preg_match('/[a-z._0-9]+[@]{1}[a-z._0-9]+[.]{1}[a-z0-9]{2,}/i', $email) === 1;
+    }
+
+    public static function main()
+    {
+        global $argc;
+        global $argv;
+        if ($argc == 2) {
+            $email = $argv[1];
+            if (self::email_validation($email)) {
+                echo "Email '$email' is valid!\n";
+            } else echo "Email '$email' is not valid!\n";
+        } else {
+            echo "Please give email address.\n";
         }
-        else echo "Email '$email' is not valid!";
-    }
-    else{
-        echo 'Please give email address.';
     }
 }
+EmailValidator::main();
