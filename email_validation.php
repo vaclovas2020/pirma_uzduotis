5394f5b13feed1ce3e562c8be1cea475c35cf1cf
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-08 10:15:08 +0300

    function email_validation changed to emailValidation

--- email_validation.php
+++ email_validation.php
@@ -4,16 +4,16 @@ namespace Validation;
 
 class EmailValidator
 {
-    private static function email_validation(string $email): bool
+    private function emailValidation(string $email): bool
     {
         return preg_match('/[a-z._0-9]+[@]{1}[a-z._0-9]+[.]{1}[a-z0-9]{2,}/i', $email) === 1;
     }
 
-    public static function main(int $argc, array $argv)
+    public function main(int $argc, array $argv)
     {
         if ($argc == 2) {
             $email = $argv[1];
-            if (self::email_validation($email)) {
+            if ($this->emailValidation($email)) {
                 echo "Email '$email' is valid!\n";
             } else echo "Email '$email' is not valid!\n";
         } else {
@@ -22,4 +22,4 @@ class EmailValidator
     }
 }
 
-EmailValidator::main($argc, $argv);
+(new EmailValidator())->main($argc, $argv);
