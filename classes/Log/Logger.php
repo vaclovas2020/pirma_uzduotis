8d50069efa7a0e974c72d568ca4d9d85f90311b0
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-08 14:20:07 +0300

    added class Logger

---
+++ classes/Log/Logger.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Log;
+
+
+class Logger implements LoggerInterface
+{
+
+    public function emergency(string $message, array $context = array()): void
+    {
+        // TODO: Implement emergency() method.
+    }
+
+    public function alert(string $message, array $context = array()): void
+    {
+        // TODO: Implement alert() method.
+    }
+
+    public function critical(string $message, array $context = array()): void
+    {
+        // TODO: Implement critical() method.
+    }
+
+    public function error(string $message, array $context = array()): void
+    {
+        // TODO: Implement error() method.
+    }
+
+    public function warning(string $message, array $context = array()): void
+    {
+        // TODO: Implement warning() method.
+    }
+
+    public function notice(string $message, array $context = array()): void
+    {
+        // TODO: Implement notice() method.
+    }
+
+    public function info(string $message, array $context = array()): void
+    {
+        // TODO: Implement info() method.
+    }
+
+    public function debug(string $message, array $context = array()): void
+    {
+        // TODO: Implement debug() method.
+    }
+
+    public function log(string $level, string $message, array $context = array()): void
+    {
+        // TODO: Implement log() method.
+    }
+}
