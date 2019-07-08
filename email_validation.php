3555d790a019fc20c1e5ae86b362698f975381b0
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-08 10:26:18 +0300

    function main changed to validate

--- email_validation.php
+++ email_validation.php
@@ -9,7 +9,7 @@ class EmailValidator
         return preg_match('/[a-z._0-9]+[@]{1}[a-z._0-9]+[.]{1}[a-z0-9]{2,}/i', $email) === 1;
     }
 
-    public function main(int $argc, array $argv)
+    public function validate(int $argc, array $argv)
     {
         if ($argc == 2) {
             $email = $argv[1];
@@ -22,4 +22,4 @@ class EmailValidator
     }
 }
 
-(new EmailValidator())->main($argc, $argv);
+(new EmailValidator())->validate($argc, $argv);
