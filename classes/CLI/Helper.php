df267bfc023a97f962e616cb63aa25c4f63ffcdb
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 13:22:12 +0300

    added Cache Storage clean command description

--- classes/CLI/Helper.php
+++ classes/CLI/Helper.php
@@ -9,5 +9,6 @@ class Helper
         echo "Use command 'php word_hyphenation.php -w [word] [save_result_to_file(optional)]' if you want to hyphenate one word.\n";
         echo "Use command 'php word_hyphenation.php -p [paragraph / sentence] [save_result_to_file(optional)]' if you want to hyphenate paragraph / sentence.\n";
         echo "Use command 'php word_hyphenation.php -f [read_file] [save_result_to_file(optional)]' if you want to hyphenate all text from given file.\n";
+        echo "Use command 'php word_hyphenation.php --clear cache' if you want to clean Cache Storage.\n";
     }
 }
