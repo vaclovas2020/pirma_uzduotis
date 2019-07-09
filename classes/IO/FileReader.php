ae90d398090c6fc64aff6f13c2d2fd535130716f
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 15:31:29 +0300

    save text file to cache

--- classes/IO/FileReader.php
+++ classes/IO/FileReader.php
@@ -7,20 +7,28 @@ namespace IO;
 use ErrorException;
 use Log\LoggerInterface;
 use Log\LogLevel;
+use SimpleCache\CacheInterface;
 
 class FileReader
 {
-    public function readTextFromFile(string $fileName, string &$text, LoggerInterface $logger): bool
+    public function readTextFromFile(string $fileName, string &$text, LoggerInterface $logger, CacheInterface $cache): bool
     {
-        try {
-            $text = @file_get_contents($fileName);
-            if ($text === false) {
-                throw new ErrorException("Cannot read text file '$fileName'!");
+        $hash = @sha1_file($fileName);
+        $cachedText = $cache->get($hash);
+        if ($cachedText === null) {
+            try {
+                $text = @file_get_contents($fileName);
+                if ($text === false) {
+                    throw new ErrorException("Cannot read text file '$fileName'!");
+                }
+            } catch (ErrorException $e) {
+                $logger->log(LogLevel::ERROR, $e->getMessage());
+                return false;
             }
-            return true;
-        } catch (ErrorException $e) {
-            $logger->log(LogLevel::ERROR, $e->getMessage());
-            return false;
+            $cache->set($hash, $text);
+        }
+        else{
+            $text = $cachedText;
         }
     }
 }
