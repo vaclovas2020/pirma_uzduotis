97c7411ddfd2fc7fc92cedf0cdddce650721efe5
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 15:20:08 +0300

    added log notice about patters list loading time and patterns count

--- classes/Hyphenation/PatternDataLoader.php
+++ classes/Hyphenation/PatternDataLoader.php
@@ -2,6 +2,8 @@
 
 namespace Hyphenation;
 
+use CLI\ExecDurationCalculator;
+use Log\LoggerInterface;
 use SimpleCache\CacheInterface;
 use SplFileObject;
 
@@ -9,11 +11,14 @@ class PatternDataLoader
 {
     public const DEFAULT_FILENAME = 'tex-hyphenation-patterns.txt';
 
-    public static function loadDataFromFile(string $filename, CacheInterface $cache): array
+    public static function loadDataFromFile(string $filename, CacheInterface $cache, LoggerInterface $logger): array
     {
         $patternData = array();
+        $execCalc = new ExecDurationCalculator();
+        $execCalc->start();
         $hash = sha1_file($filename);
         $cachedData = $cache->get($hash);
+        $source = "from file '$filename'";
         if ($cachedData === null) {
             $file = new SplFileObject($filename, 'r');
             while (!$file->eof()) {
@@ -23,7 +28,17 @@ class PatternDataLoader
             $cache->set($hash, $patternData);
         } else {
             $patternData = $cachedData;
+            $source = 'from cache';
         }
+        $execCalc->finish();
+        $execDuration = $execCalc->getDuration();
+        $logger->notice("Patterns list read {from} time: {execDuration} seconds", array(
+            'execDuration' => $execDuration,
+            'from' => $source
+        ));
+        $logger->notice("{count} patterns loaded to memory", array(
+            'count' => count($patternData)
+        ));
         return $patternData;
     }
 }
