a1933aa96932b3654af39c19d3723586b33f9d2e
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 11:35:41 +0300

    modified interface CacheInterface

--- classes/SimpleCache/CacheInterface.php
+++ classes/SimpleCache/CacheInterface.php
@@ -6,20 +6,20 @@ namespace SimpleCache;
 
 interface CacheInterface
 {
-    public function get(string $key, $default = null): mixed;
+    public function get(string $key, $default = null);
 
-    public function set(string $key, mixed $value, int $ttl = null): bool;
+    public function set(string $key, $value, int $ttl = null): bool;
 
-    public function delete($key): bool;
+    public function delete(string $key): bool;
 
     public function clear(): bool;
 
-    public function getMultiple(array $keys, $default = null): bool;
+    public function getMultiple(array $keys, $default = null): array;
 
     public function setMultiple(array $values, int $ttl = null): bool;
 
     public function deleteMultiple(array $keys): bool;
 
-    public function has($key);
+    public function has(string $key): bool;
 
 }
