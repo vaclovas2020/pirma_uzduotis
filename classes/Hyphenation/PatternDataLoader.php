909c5c75d5d93b1af51eaf5ebba3786c38d3377c
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 15:04:36 +0300

    added Pattern file cache

--- classes/Hyphenation/PatternDataLoader.php
+++ classes/Hyphenation/PatternDataLoader.php
@@ -2,20 +2,29 @@
 
 namespace Hyphenation;
 
+use SimpleCache\CacheInterface;
 use SplFileObject;
 
 class PatternDataLoader
 {
     public const DEFAULT_FILENAME = 'tex-hyphenation-patterns.txt';
 
-    public static function loadDataFromFile(string $filename): array
+    public static function loadDataFromFile(string $filename, CacheInterface $cache): array
     {
         $patternData = array();
-        $file = new SplFileObject($filename, 'r');
-        while (!$file->eof()) {
-            array_push($patternData, str_replace("\n", '', $file->fgets()));
+        $hash = sha1_file($filename);
+        $cachedData = $cache->get($hash);
+        if ($cachedData === null) {
+            $file = new SplFileObject($filename, 'r');
+            while (!$file->eof()) {
+                array_push($patternData, str_replace("\n", '', $file->fgets()));
+            }
+            $file = null;
+            $cache->set($hash,$patternData);
         }
-        $file = null;
+        else{
+            $patternData = $cachedData;
+        }
         return $patternData;
     }
 }
