f51d074498393d13986d47947d9bdfda09a83931
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-09 13:19:08 +0300

    added parameter CLI '--clear cache' for clean Cache Storage

--- classes/CLI/UserInput.php
+++ classes/CLI/UserInput.php
@@ -8,30 +8,43 @@ use Hyphenation\PatternDataLoader;
 use Hyphenation\WordHyphenationTool;
 use IO\FileReader;
 use Log\LoggerInterface;
+use SimpleCache\CacheInterface;
 use SimpleCache\FileCache;
 
 class UserInput
 {
-    public function textHyphenationUI(string $choice, string $input, string &$resultStr, LoggerInterface $logger): bool
+    public function textHyphenationUI(string $choice, string $input, string &$resultStr, LoggerInterface $logger, CacheInterface $cache): bool
     {
-        $hyphenationTool = new WordHyphenationTool($logger, new FileCache());
+        $hyphenationTool = new WordHyphenationTool($logger, $cache);
         $allPatterns = PatternDataLoader::loadDataFromFile(PatternDataLoader::DEFAULT_FILENAME);
         switch ($choice) {
             case '-w': // hyphenate one word
-                $logger->info("Chosen hyphenate one word '{word}'", array('word'=>$input));
+                $logger->info("Chosen hyphenate one word '{word}'", array('word' => $input));
                 $resultStr = $hyphenationTool->oneWordHyphenation($allPatterns, $input);
                 break;
             case '-p': // hyphenate all paragraph or one sentence
-                $logger->info("Chosen hyphenate paragraph /sentence '{text}'", array('text'=>$input));
+                $logger->info("Chosen hyphenate paragraph /sentence '{text}'", array('text' => $input));
                 $resultStr = $hyphenationTool->hyphenateAllText($allPatterns, $input);
                 break;
             case '-f': // hyphenate all text from given file
-                $logger->info("Chosen hyphenate from text file '{filename}'", array('filename'=>$input));
+                $logger->info("Chosen hyphenate from text file '{filename}'", array('filename' => $input));
                 $status = (new FileReader)->readTextFromFile($input, $resultStr, $logger);
                 if ($status === false) {
                     return false;
                 }
                 $resultStr = $hyphenationTool->hyphenateAllText($allPatterns, $resultStr);
+                break;
+            case '--clear':
+                if ($input == 'cache'){
+                    if ($cache->clear()){
+                        $logger->notice('Cache Storage was cleaned.');
+                    }
+                    else{
+                        $logger->error('Cannot clean Cache Storage');
+                    }
+                }
+                else $logger->warning("Unknown storage named '{input}'.", array('input' => $input));
+                return false;
                 break;
             default:
                 $logger->warning("Unknown {choice} parameter.", array('choice' => $choice));
