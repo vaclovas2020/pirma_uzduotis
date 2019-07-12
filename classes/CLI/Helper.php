6b609ff54bac8ce11ff6db284e1c9cb1add188c5
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-12 13:37:26 +0300

    added new line

--- classes/CLI/Helper.php
+++ classes/CLI/Helper.php
@@ -7,7 +7,7 @@ class Helper
     public function printHelp(): void
     {
         echo "Use command 'php word_hyphenation.php --config-db [db_host] [db_name] [db_user] [db_password]' if you want to configure database.\n";
-        echo "Use command ' php word_hyphenation.php --db-import-patterns-file [pattern_file_path]' if you want to import patterns file to database.";
+        echo "Use command ' php word_hyphenation.php --db-import-patterns-file [pattern_file_path]' if you want to import patterns file to database.\n";
         echo "Use command 'php word_hyphenation.php -w [word] [save_result_to_file(optional)]' if you want to hyphenate one word.\n";
         echo "Use command 'php word_hyphenation.php -p [paragraph / sentence] [save_result_to_file(optional)]' if you want to hyphenate paragraph / sentence.\n";
         echo "Use command 'php word_hyphenation.php -f [read_file] [save_result_to_file(optional)]' if you want to hyphenate all text from given file.\n";
