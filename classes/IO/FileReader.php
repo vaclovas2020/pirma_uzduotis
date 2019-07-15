10c6259e4c36a21ebad272e0b2859bc99d7c1f31
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-15 15:39:07 +0300

    check if file exists

--- classes/IO/FileReader.php
+++ classes/IO/FileReader.php
@@ -23,30 +23,36 @@ class FileReader
 
     public function readTextFromFile(string $fileName, string &$text): bool
     {
-        $execCalc = new ExecDurationCalculator();
-        $hash = @sha1_file($fileName);
-        $cachedText = $this->cache->get($hash);
-        $source = "from file '$fileName'";
-        if ($cachedText === null) {
-            try {
-                $text = @file_get_contents($fileName);
-                if ($text === false) {
-                    throw new ErrorException("Cannot read text file '$fileName'!");
+        if (file_exists($fileName)) {
+            $execCalc = new ExecDurationCalculator();
+            $hash = @sha1_file($fileName);
+            $cachedText = $this->cache->get($hash);
+            $source = "from file '$fileName'";
+            if ($cachedText === null) {
+                try {
+                    $text = @file_get_contents($fileName);
+                    if ($text === false) {
+                        throw new ErrorException("Cannot read text file '$fileName'!");
+                    }
+                } catch (ErrorException $e) {
+                    $this->logger->log(LogLevel::ERROR, $e->getMessage());
+                    return false;
                 }
-            } catch (ErrorException $e) {
-                $this->logger->log(LogLevel::ERROR, $e->getMessage());
-                return false;
+                $this->cache->set($hash, $text);
+            } else {
+                $text = $cachedText;
+                $source = 'from cache';
             }
-            $this->cache->set($hash, $text);
-        } else {
-            $text = $cachedText;
-            $source = 'from cache';
+            $execDuration = $execCalc->finishAndGetDuration();
+            $this->logger->notice("Text read {from} time: {execDuration} seconds", array(
+                'execDuration' => $execDuration,
+                'from' => $source
+            ));
         }
-        $execDuration = $execCalc->finishAndGetDuration();
-        $this->logger->notice("Text read {from} time: {execDuration} seconds", array(
-            'execDuration' => $execDuration,
-            'from' => $source
-        ));
+        else{
+            $this->logger->error('File {fileName} not found!', array('fileName'=>$fileName));
+            return false;
+        }
         return true;
     }
 }
