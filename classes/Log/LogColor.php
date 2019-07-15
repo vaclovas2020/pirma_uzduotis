ac13fa2b3a7445f02bb2617dcc12d13437e132b8
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-15 15:11:21 +0300

    added class LogColor

---
+++ classes/Log/LogColor.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Log;
+
+
+class LogColor
+{
+    public const EMERGENCY = 'purple;white';
+    public const ALERT = 'light_red;black';
+    public const CRITICAL = 'red;white';
+    public const ERROR = 'red;black';
+    public const WARNING = 'yellow;black';
+    public const NOTICE = 'white;green';
+    public const INFO = 'white;blue';
+    public const DEBUG = 'black;white';
+}
