9bbe0494d9e7a14180137d37e0c7c15f2c0df276
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-16 10:43:19 +0300

    added bold style

--- classes/Log/LogColor.php
+++ classes/Log/LogColor.php
@@ -6,12 +6,12 @@ namespace Log;
 
 class LogColor
 {
-    public const EMERGENCY = "\033[35m%s\033[0m";
-    public const ALERT = "\033[31m%s\033[0m";
-    public const CRITICAL = "\033[31m%s\033[0m";
-    public const ERROR = "\033[31m%s\033[0m";
-    public const WARNING = "\033[33m%s\033[0m";
-    public const NOTICE = "\033[32m%s\033[0m";
-    public const INFO = "\033[34m%s\033[0m";
-    public const DEBUG = "\033[37m%s\033[0m";
+    public const EMERGENCY = "\033[1;35m%s\033[0m";
+    public const ALERT = "\033[1;31m%s\033[0m";
+    public const CRITICAL = "\033[1;31m%s\033[0m";
+    public const ERROR = "\033[1;31m%s\033[0m";
+    public const WARNING = "\033[1;33m%s\033[0m";
+    public const NOTICE = "\033[1;32m%s\033[0m";
+    public const INFO = "\033[1;34m%s\033[0m";
+    public const DEBUG = "\033[1;37m%s\033[0m";
 }
