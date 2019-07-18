cd0c6ade112fc865f5360aa7fb896ca506c01e58
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-18 13:36:46 +0300

    use different class path when php script is running at server

--- classes/Core/AutoLoader.php
+++ classes/Core/AutoLoader.php
@@ -4,13 +4,23 @@ namespace Core;
 
 class AutoLoader
 {
-    public static function register()
+    public static function register(bool $isInServer = false)
     {
-        spl_autoload_register(function (string $class) {
-            $class = str_replace('\\', '/', $class);
-            if (file_exists("classes/$class.php")) {
-                include "classes/$class.php";
-            }
-        });
+        if ($isInServer){
+            spl_autoload_register(function (string $class) {
+                $class = str_replace('\\', '/', $class);
+                if (file_exists($_SERVER['DOCUMENT_ROOT']."/classes/$class.php")) {
+                    include $_SERVER['DOCUMENT_ROOT']."/classes/$class.php";
+                }
+            });
+        }
+        else {
+            spl_autoload_register(function (string $class) {
+                $class = str_replace('\\', '/', $class);
+                if (file_exists("classes/$class.php")) {
+                    include "classes/$class.php";
+                }
+            });
+        }
     }
 }
