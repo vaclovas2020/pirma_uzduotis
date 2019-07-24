d2ebc8350ee29ad6e89c229045e58ade76fc17fd
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-24 11:16:28 +0300

    code clean up

--- classes/CLI/App.php
+++ classes/CLI/App.php
@@ -34,49 +34,22 @@ class App
 
     public function checkConfigurationCLI(int $argc, array $argv): bool
     {
-        if ($this->isDatabaseConfigurationCliArguments($argc, $argv)) {
-            $this->config->configureDatabase(
-                $argv[self::DB_HOST_ARGC], $argv[self::DB_NAME_ARGC],
-                $argv[self::DB_USER_ARGC], $argv[self::DB_PASSWORD_ARGC]);
-            return true;
-        } else if ($argc == self::CLI_MINIMUM_ARGC && $argv[self::CLI_ACTION_ARGC] === '--db-import-patterns-file') {
-            $dbPatterns = new DbPatterns($this->config, $this->logger, $this->cache);
-            if ($dbPatterns->importFromFile($argv[self::CLI_FILENAME_ARGC])) {
-                $this->logger->notice('Patterns file {fileName} successfully imported to database!',
-                    array('fileName' => $argv[self::CLI_FILENAME_ARGC]));
-            } else {
-                $this->logger->error('Patterns file {fileName} was not imported to database because error occurred!',
-                    array('fileName' => $argv[self::CLI_FILENAME_ARGC]));
-            }
-            return true;
-        } else if ($argc == self::CLI_MINIMUM_ARGC && $argv[self::CLI_ACTION_ARGC] === '--clear') {
-            $this->userInput->clearStorage($argv[self::CLI_FILENAME_ARGC]);
-            return true;
-        }
-        return false;
+        return $this->checkDatabaseConfiguration($argc, $argv) ||
+            $this->checkImportPatternsFileToDb($argc, $argv) ||
+            $this->checkClearStorage($argc, $argv);
     }
 
     public function start(int $argc, array $argv): void
     {
-        $this->logger->debug('Program started with arguments: {arguments}',
-            array('arguments' => $argv));
         $checkConfigurationCli = $this->checkConfigurationCLI($argc, $argv);
         if (!$checkConfigurationCli && $argc >= self::CLI_MINIMUM_ARGC) {
             $execCalc = new ExecDurationCalculator();
             $choose = $argv[self::CLI_ACTION_ARGC]; // -w one word, -p paragraph, -f file
             $resultStr = '';
             $status = $this->userInput->processInput($choose, $argv[self::CLI_FILENAME_ARGC], $resultStr);
-            if ($status !== false) {
-                $fileName = '';
-                $writeToFile = false;
-                $fileWriter = new FileWriter();
-                $userInput = new UserOutput($this->logger, $fileWriter);
-                if ($argc > self::CLI_MINIMUM_ARGC) {
-                    $fileName = $argv[self::CLI_MINIMUM_ARGC];
-                    $writeToFile = true;
-                }
-                $userInput->outputToUser($resultStr, $writeToFile, $fileName);
-            }
+            $writeToFile = $argc > self::CLI_MINIMUM_ARGC;
+            $fileName = ($writeToFile) ? $argv[self::CLI_MINIMUM_ARGC] : '';
+            $this->outputToUser($resultStr, $status, $fileName, $writeToFile);
             $execDuration = $execCalc->finishAndGetDuration();
             $this->logger->notice("Program execution duration: {execDuration} seconds", array(
                 'execDuration' => $execDuration
@@ -86,9 +59,61 @@ class App
         }
     }
 
-    private function isDatabaseConfigurationCliArguments(int $argc, array $argv)
+    private function isDatabaseConfigurationCliArguments(int $argc, array $argv): bool
     {
         return $argc === self::DB_CONFIGURATION_ARGC && $argv[self::CLI_ACTION_ARGC] === '--config-db';
+    }
+
+    private function isImportPatternsFileToDbCliArguments(int $argc, array $argv)
+    {
+        return $argc === self::CLI_MINIMUM_ARGC && $argv[self::CLI_ACTION_ARGC] === '--db-import-patterns-file';
+    }
+
+    private function importPatternsFileToDb(string $fileName)
+    {
+        $dbPatterns = new DbPatterns($this->config, $this->logger, $this->cache);
+        if ($dbPatterns->importFromFile($fileName)) {
+            $this->logger->notice('Patterns file {fileName} successfully imported to database!',
+                array('fileName' => $fileName));
+        } else {
+            $this->logger->error('Patterns file {fileName} was not imported to database because error occurred!',
+                array('fileName' => $fileName));
+        }
+    }
+
+    private function checkDatabaseConfiguration(int $argc, array $argv): bool
+    {
+        if ($this->isDatabaseConfigurationCliArguments($argc, $argv)) {
+            $this->config->configureDatabase(
+                $argv[self::DB_HOST_ARGC], $argv[self::DB_NAME_ARGC],
+                $argv[self::DB_USER_ARGC], $argv[self::DB_PASSWORD_ARGC]);
+            return true;
+        } else return false;
+    }
+
+    private function checkImportPatternsFileToDb(int $argc, array $argv): bool
+    {
+        if ($this->isImportPatternsFileToDbCliArguments($argc, $argv)) {
+            $this->importPatternsFileToDb($argv[self::CLI_FILENAME_ARGC]);
+            return true;
+        } else return false;
+    }
+
+    private function checkClearStorage(int $argc, array $argv): bool
+    {
+        if ($argc == self::CLI_MINIMUM_ARGC && $argv[self::CLI_ACTION_ARGC] === '--clear') {
+            $this->userInput->clearStorage($argv[self::CLI_FILENAME_ARGC]);
+            return true;
+        } else return false;
+    }
+
+    private function outputToUser(string $resultStr, bool $status, string $fileName, bool $writeToFile): void
+    {
+        if ($status !== false) {
+            $fileWriter = new FileWriter();
+            $userInput = new UserOutput($this->logger, $fileWriter);
+            $userInput->outputToUser($resultStr, $writeToFile, $fileName);
+        }
     }
 
 }
