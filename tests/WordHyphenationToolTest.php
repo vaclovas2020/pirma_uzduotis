56db5a4cee0602461b63e8d2cfbce55766218f3e
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-25 17:25:10 +0300

    use creatMock method for every object

--- tests/WordHyphenationToolTest.php
+++ tests/WordHyphenationToolTest.php
@@ -12,11 +12,15 @@ class WordHyphenationToolTest extends TestCase
 
     public function testHyphenateWord()
     {
-        $logger = new Logger();
-        $cache = new FileCache();
-        $config = new Config($logger);
+        $logger = $this->createMock(Logger::class);
+        $cache = $this->createMock(FileCache::class);
+        $config = $this->createMock(Config::class);
         $hyphenationTool = new WordHyphenationTool($logger, $cache, $config);
         $this->assertEquals('mis-trans-late', $hyphenationTool->hyphenateWord('mistranslate'),
             "Test failed: mistranslate not hyphenated to mis-trans-late");
+        $this->assertEquals('net-work', $hyphenationTool->hyphenateWord('network'),
+            "Test failed: network not hyphenated to net-work");
+        $this->assertEquals('cat-fish', $hyphenationTool->hyphenateWord('catfish'),
+            "Test failed: catfish not hyphenated to cat-fish");
     }
 }
