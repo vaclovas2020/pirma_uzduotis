ac594cf3b1195b458d64841775ea38d63dda0c20
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-25 16:59:35 +0300

    added WordHyphenationToolTest

--- tests/WordHyphenationToolTest.php
+++ tests/WordHyphenationToolTest.php
@@ -1,9 +1,22 @@
 <?php
 
 
+use AppConfig\Config;
+use Hyphenation\WordHyphenationTool;
+use Log\Logger;
 use PHPUnit\Framework\TestCase;
+use SimpleCache\FileCache;
 
 class WordHyphenationToolTest extends TestCase
 {
 
+    public function testHyphenateWord()
+    {
+        $logger = new Logger();
+        $cache = new FileCache();
+        $config = new Config($logger);
+        $hyphenationTool = new WordHyphenationTool($logger, $cache, $config);
+        $this->assertEquals('mis-trans-late', $hyphenationTool->hyphenateWord('mistranslate'),
+            "Test failed: mistranslate not hyphenated to mis-trans-late");
+    }
 }
