a5fad3ed9edac9d3d586a399f5c194d34c2b4ab6
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2019-07-26 08:59:54 +0300

    added willReturn that method getDbConfig return DbConfig object with correct database configuration

--- tests/WordHyphenationToolTest.php
+++ tests/WordHyphenationToolTest.php
@@ -2,6 +2,7 @@
 
 
 use AppConfig\Config;
+use AppConfig\DbConfig;
 use Hyphenation\WordHyphenationTool;
 use Log\Logger;
 use PHPUnit\Framework\TestCase;
@@ -10,11 +11,19 @@ use SimpleCache\FileCache;
 class WordHyphenationToolTest extends TestCase
 {
 
-    public function testHyphenateWord()
+    public function testHyphenateWord(): void
     {
         $logger = $this->createMock(Logger::class);
         $cache = $this->createMock(FileCache::class);
         $config = $this->createMock(Config::class);
+        $config
+            ->method('getDbConfig')
+            ->willReturn(DbConfig::getInstance(
+                'localhost',
+                'word_hyphenation_db',
+                'root',
+                'Q1w5e9r7t5y3@',
+                $logger, true));
         $hyphenationTool = new WordHyphenationTool($logger, $cache, $config);
         $this->assertEquals('mis-trans-late', $hyphenationTool->hyphenateWord('mistranslate'),
             "Test failed: mistranslate not hyphenated to mis-trans-late");
@@ -22,5 +31,23 @@ class WordHyphenationToolTest extends TestCase
             "Test failed: network not hyphenated to net-work");
         $this->assertEquals('cat-fish', $hyphenationTool->hyphenateWord('catfish'),
             "Test failed: catfish not hyphenated to cat-fish");
+    }
+
+    public function testGetFoundPatternsOfWord(): void
+    {
+        $logger = $this->createMock(Logger::class);
+        $cache = $this->createMock(FileCache::class);
+        $config = $this->createMock(Config::class);
+        $config
+            ->method('getDbConfig')
+            ->willReturn(DbConfig::getInstance(
+                'localhost',
+                'word_hyphenation_db',
+                'root',
+                'Q1w5e9r7t5y3@',
+                $logger, true));
+        $hyphenationTool = new WordHyphenationTool($logger, $cache, $config);
+        $this->assertNotEmpty($hyphenationTool->getFoundPatternsOfWord('mistranslate'),
+            "getFoundPatternsOfWord return empty array");
     }
 }
